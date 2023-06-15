d84a55898d4ad9ec2172ab294e40de5c1517f116
James Andrew <james@globalliquidity.io>
2023-06-15 14:23:41 -0700

    Adding schema to feeds query

--- pkg/events/events.go
+++ pkg/events/events.go
@@ -2,19 +2,20 @@ package events
 
 import (
 	"database/sql"
+	"log"
+	"net/url"
+	"strings"
+
 	"github.com/mmcdole/gofeed"
 	"github.com/piraces/rsslay/pkg/feed"
 	"github.com/piraces/rsslay/pkg/helpers"
 	"github.com/piraces/rsslay/pkg/metrics"
 	"github.com/prometheus/client_golang/prometheus"
-	"log"
-	"net/url"
-	"strings"
 )
 
 func GetParsedFeedForPubKey(pubKey string, db *sql.DB, deleteFailingFeeds bool, nitterInstances []string) (*gofeed.Feed, feed.Entity) {
 	pubKey = strings.TrimSpace(pubKey)
-	row := db.QueryRow("SELECT privatekey, url, nitter FROM feeds WHERE publickey=$1", pubKey)
+	row := db.QueryRow("SELECT privatekey, url, nitter FROM public.feeds WHERE publickey=$1", pubKey)
 
 	var entity feed.Entity
 	err := row.Scan(&entity.PrivateKey, &entity.URL, &entity.Nitter)
